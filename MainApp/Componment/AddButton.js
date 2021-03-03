4fbb6f037e2ee2fd3fcef89a7a914befdfaaa9bc
Timothy <timothylam1228@gmail.com>
2021-03-04 02:24:51 +0800

    add font to android (not done)
    apply from: "../../node_modules/react-native-vector-icons/fonts.gradle"

---
+++ MainApp/Componment/AddButton.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet,View, Text, ImageBackground,TouchableOpacity,Image, TouchableOpacityBase} from 'react-native';
+
+export default function AddButton () {
+
+    return (
+      <View>
+        <Text>asd</Text>
+        <Text>asd</Text>
+        <Text>asd</Text>
+        <Text>asd</Text>
+        <Text>asd</Text>
+    </View>
+    );
+}
